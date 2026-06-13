fcb8d615a3293784c4b5309bd983faec7e5860c4
thessrunited <thessrunited@gmail.com>
2026-06-14 02:06:14 +0530

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders StencilCode redesign content', () => {
-  render(<App />);
-  expect(screen.getByText(/We engineer solutions. Thoughtfully./i)).toBeInTheDocument();
-  expect(screen.getByText(/Full-Stack Web Development/i)).toBeInTheDocument();
-});
